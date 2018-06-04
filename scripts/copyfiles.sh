6f79a1b0f139fc4786bea04f969b5af38bbe15db
Jonas <ge69jum@mytum.de>
2018-06-04 11:51:01 +0200

    added copyfiles script, which copies files to the robot

---
+++ scripts/copyfiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Exit immediately if something fails
+set -e
+
+
+if [ -z "$1" ] 
+then
+	nao_ip=192.168.0.11
+else
+	nao_ip=$1
+fi
+
+sshpass -p 'nao' ssh nao@$nao_ip 'cd ~ && rm ball_approach.py utils.py finders.py movements.py imagereaders.py nao_defaults.json || echo "already removed"'
+
+
+# copy the files with scp
+sshpass -p 'nao' scp ball_approach.py  nao@$nao_ip:~
+sshpass -p 'nao' scp utils.py  nao@$nao_ip:~
+sshpass -p 'nao' scp finders.py  nao@$nao_ip:~
+sshpass -p 'nao' scp movements.py  nao@$nao_ip:~
+sshpass -p 'nao' scp imagereaders.py nao@$nao_ip:~
+
+
+
+# config ersetzen
+sshpass -p 'nao' scp nao_defaults.json   nao@192.168.0.11:~
+
